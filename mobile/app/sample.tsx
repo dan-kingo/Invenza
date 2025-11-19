5bcdef5e1a92c3b494cfffe0597b7a1ab514aefb
Dan Kingo <danieldejen23@gmail.com>
2025-11-19 19:28:53 +0300

    initalize mobile app

---
+++ mobile/app/sample.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class sample extends Component {
+  render() {
+    return (
+      <View>
+        <Text>sample</Text>
+      </View>
+    )
+  }
+}
+
+export default sample
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ["./app/**/*.{tsx,ts,js,jsx}"],
+  presets: [require("nativewind/preset")],
+};
